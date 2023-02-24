2bcaa10cd0a4e182a19db58f9e237c41456c3925
José Nelson <josefcultrifrc@gmail.com>
2023-02-24 08:21:54 -0300

    Modificando método de busca para retornar uma Task de maneira assíncrona

--- Data/QuotationEngine.cs
+++ Data/QuotationEngine.cs
@@ -1,12 +1,21 @@
+using AngleSharp;
+
 namespace MagicCotador;
 
 public class QuotationEngine
 {
-    public Card GetCardData(string _cardName)
+    public async Task<Card> GetCardData(string _cardName)
     {
         Card _returnCard = new Card(_cardName);
 
         //TODO: Executar busca na ligamagic e processar as lojas
+        /*
+        var config = Configuration.Default.WithDefaultLoader();
+        using var context = BrowsingContext.New(config);
+        using var doc = await context.OpenAsync("https://www.ligamagic.com.br/?view=cards/card&card=platinum%20angel");
+        //cardName = doc.QuerySelector("p.nome-principal").GetAttribute("title");
+        cardName = doc.DocumentElement.QuerySelector("p.nome-principal > span").InnerHtml;
+        */
 
         return _returnCard;
     }
